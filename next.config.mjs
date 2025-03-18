359309b6c6540b9a8696578e3b700c913c47b588
Daniel Ramirez <danielramirez.wm@gmail.com>
2025-03-18 13:24:56 -0500

    fix: retire basepath

--- next.config.mjs
+++ next.config.mjs
@@ -20,8 +20,7 @@ const nextConfig = {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
-  },
-  basePath: "/proyecto"
+  }
 }
 
 mergeConfig(nextConfig, userConfig)
